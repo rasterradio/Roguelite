0b5805784eb5f5c4db33b145d81d88e274e52aea
rasterradio <funnyguy150@gmail.com>
2016-11-21 22:03:56 -0500

    Removed dungeon generation parameters

--- roguelite.py
+++ roguelite.py
@@ -6,13 +6,7 @@ SCREEN_HEIGHT = 50
  
 #size of the map
 MAP_WIDTH = 80
-MAP_HEIGHT = 45
- 
-#parameters for dungeon generator
-ROOM_MAX_SIZE = 10
-ROOM_MIN_SIZE = 6
-MAX_ROOMS = 30
- 
+MAP_HEIGHT = 45 
  
 FOV_ALGO = 0  #default FOV algorithm
 FOV_LIGHT_WALLS = False  #light walls or not
@@ -134,91 +128,19 @@ class Landmark(Object):
                 #run event if available
                 self.event.run()
                                  
-def create_room(room):
-    global map
-    #go through the tiles in the rectangle and make them passable
-    for x in range(room.x1 + 1, room.x2):
-        for y in range(room.y1 + 1, room.y2):
-            map[x][y].blocked = False
-            map[x][y].block_sight = False
- 
-def create_h_tunnel(x1, x2, y):
-    global map
-    #horizontal tunnel. min() and max() are used in case x1>x2
-    for x in range(min(x1, x2), max(x1, x2) + 1):
-        map[x][y].blocked = False
-        map[x][y].block_sight = False
- 
-def create_v_tunnel(y1, y2, x):
-    global map
-    #vertical tunnel
-    for y in range(min(y1, y2), max(y1, y2) + 1):
-        map[x][y].blocked = False
-        map[x][y].block_sight = False
- 
 def make_map():
-    global map, player
+    global map
  
-    #fill map with "blocked" tiles
-    map = [[ Tile(True)
+    #fill map with "unblocked" tiles
+    map = [[ Tile(False)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
  
-    rooms = []
-    num_rooms = 0
- 
-    for r in range(MAX_ROOMS):
-        #random width and height
-        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        #random position without going out of the boundaries of the map
-        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
-        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
- 
-        #"Rect" class makes rectangles easier to work with
-        new_room = Rect(x, y, w, h)
- 
-        #run through the other rooms and see if they intersect with this one
-        failed = False
-        for other_room in rooms:
-            if new_room.intersect(other_room):
-                failed = True
-                break
- 
-        if not failed:
-            #this means there are no intersections, so this room is valid
- 
-            #"paint" it to the map's tiles
-            create_room(new_room)
- 
-            #center coordinates of new room, will be useful later
-            (new_x, new_y) = new_room.center()
- 
-            if num_rooms == 0:
-                #this is the first room, where the player starts at
-                player.x = new_x
-                player.y = new_y
-            else:
-                #all rooms after the first:
-                #connect it to the previous room with a tunnel
- 
-                #center coordinates of previous room
-                (prev_x, prev_y) = rooms[num_rooms-1].center()
- 
-                #draw a coin (random number that is either 0 or 1)
-                if libtcod.random_get_int(0, 0, 1) == 1:
-                    #first move horizontally, then vertically
-                    create_h_tunnel(prev_x, new_x, prev_y)
-                    create_v_tunnel(prev_y, new_y, new_x)
-                else:
-                    #first move vertically, then horizontally
-                    create_v_tunnel(prev_y, new_y, prev_x)
-                    create_h_tunnel(prev_x, new_x, new_y)
- 
-            #finally, append the new room to the list
-            rooms.append(new_room)
-            num_rooms += 1
- 
+    #place two pillars to test the map
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
  
 def render_all():
     global fov_map, color_dark_wall, color_light_wall
