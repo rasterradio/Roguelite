f158a0ec3c63393d78656d88afb47e5884501d46
= <diggin.wilson@gmail.com>
2016-12-31 16:23:12 -0500

    added HelperFunctions

--- roguelite.py
+++ roguelite.py
@@ -260,22 +260,6 @@ def make_map():
 
     map_data.close()
 
-def read_grid_text(file, xRange, yRange):
-    text_file = open(file, 'r')
-    text_data = [["" for a in range(yRange)] for b in range(xRange)]
-    x = 0
-    y = 0
-    for line in text_file:
-        for word in line.split(';'):
-            if x < xRange and y < yRange:
-                text_data[x][y] = word.replace("^", "\n")
-                
-                #print file + " " + str(x) + " " + str(y) + " " + text_data[x][y]
-            x += 1
-        x = 0
-        y += 1
-    return text_data
-    
 #############################################
 # Initialization & Main Loop
 #############################################
